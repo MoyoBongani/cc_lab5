c64687fe21a5c082af75ed8620fcae4d1b263b4d
MoyoBongani <C20309081@mytudublin.ie>
2022-12-12 21:07:55 +0000

    original code

--- template/addPage.php
+++ template/addPage.php
@@ -6,8 +6,19 @@
 
 <body>
 
-<h1> Add Student </h1>]
+<h1> Add Student </h1>
 
+<form method="POST">
+    <p>Name:
+    <input type="text" name="pname" id="pname"></p>
+
+    <p>Email:
+    <input type="text" name="desc" id="desc"></p>
+
+    <p><input type="submit" value="Add New"/></p>
+</form>
+
+<a href="index.html"> Go Back to Home </a>
 
 </body>
 </html>
--- template/deletePage.php
+++ template/deletePage.php
@@ -6,7 +6,14 @@
 
 <body>
 
-<h1> Delete Student </h1>]
+<h1> Delete Student </h1>
+
+<form method="POST">
+    
+    <p>Name:
+    <input type="text" name="pname" id="pname"></p>
+
+    <p><input type="submit" value="Delete Student"/></p>
 
 
 </body>
